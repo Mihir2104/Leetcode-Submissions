06f470b1e8566003a7fa1586ecb9248fa3623a66
Mihir2104 <63975338+Mihir2104@users.noreply.github.com>
2026-08-05 00:26:13 +0530

    Time: 6 ms (43.01%), Space: 44.1 MB (95.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,21 +4,23 @@ class Solution {
             return false;
         }
 
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        int freq[] = new int [26];
 
-        for(int i = 0; i < s.length() ; i ++){
-            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
-            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+        for(int i = 0 ; i < s.length(); i++ ){
+
+            int first = s.charAt(i) - 'a';
+            int second = t.charAt(i) - 'a';
+
+            freq[first]++;
+            freq[second]--;
         }
 
-        for(int i = 0; i < s.length(); i ++){
-            if(!map1.get(s.charAt(i)).equals(map2.get(s.charAt(i)))){
+        for(int i =0; i < 26 ; i++){
+            if(freq[i]!= 0){
                 return false;
             }
         }
-
-        return true;
         
+        return true;
     }
 }
